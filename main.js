a81b0ef76c7847b07409bd50a4da8cc59210ace1
cebopawel <cebo.pawel@gmail.com>
2018-09-01 13:10:25 +0200

    final

--- main.js
+++ main.js
@@ -10,29 +10,28 @@ const pg = document.querySelector(".playground");
 
 gameTimer=70;
 
+function getRandomNo (){
+    rand = Math.floor(Math.random()*49)*10;
+    return rand;
+}
 function gameOver (){
-    points = 0;
-    PositionX=10;
-    PositionY=10;
-    yDirection.push(0);
-    xDirection.push(0);
     location.reload();
 }
 
 document.addEventListener('keydown', function(event) {
-    if (event.code == 'KeyW') {
+    if ((event.code == 'KeyW') || (event.code == 'ArrowUp')){
         yDirection.push(-10);
         xDirection.push(0);
     }
-    if (event.code == 'KeyS') {
+    if ((event.code == 'KeyS') || (event.code == 'ArrowDown')){
         yDirection.push(10);
         xDirection.push(0);
     }
-    if (event.code == 'KeyA') {
+    if ((event.code == 'KeyA') || (event.code == 'ArrowLeft')){
         xDirection.push(-10);
         yDirection.push(0) ; 
     }
-    if (event.code == 'KeyD') {
+    if ((event.code == 'KeyD') || (event.code == 'ArrowRight')){
         xDirection.push(10); 
         yDirection.push(0);
     }
@@ -50,15 +49,16 @@ document.addEventListener('keydown', function(event) {
 
    var snakeHead = document.createElement("div");
    pg.appendChild(snakeHead);
+
    var monster = document.createElement("div");
    monster.classList.add("monster");
    pg.appendChild(monster);
-   var monsterPositionX=Math.floor(Math.random()*49)*10;
-   var monsterPositionY=Math.floor(Math.random()*49)*10;
+   var monsterPositionX=getRandomNo ();
+   var monsterPositionY=getRandomNo ();
    monster.style.setProperty("top", monsterPositionY +"px" );
    monster.style.setProperty("left", monsterPositionX +"px" );
 
-   var monster2 = document.createElement("div");
+    var monster2 = document.createElement("div");
     monster2.classList.add("monster");
     var monster3 = document.createElement("div");
     monster3.classList.add("monster");
@@ -66,30 +66,29 @@ document.addEventListener('keydown', function(event) {
     monster4.classList.add("monster");
     monster4.classList.add("bad-monster");
 
-   // MORE MONSTERS 
-   var monsterPositionX2=Math.floor(Math.random()*49)*10;
-   var monsterPositionY2=Math.floor(Math.random()*49)*10;
+   var monsterPositionX2=getRandomNo ();
+   var monsterPositionY2=getRandomNo ();
    monster2.style.setProperty("top", monsterPositionY2 +"px" );
    monster2.style.setProperty("left", monsterPositionX2 +"px" );
-   var monsterPositionX3=Math.floor(Math.random()*49)*10;
-   var monsterPositionY3=Math.floor(Math.random()*49)*10;
+   var monsterPositionX3=getRandomNo ();
+   var monsterPositionY3=getRandomNo ();
    monster3.style.setProperty("top", monsterPositionY3 +"px" );
    monster3.style.setProperty("left", monsterPositionX3 +"px" );
-   var monsterPositionX4=Math.floor(Math.random()*49)*10;
-   var monsterPositionY4=Math.floor(Math.random()*49)*10;
+   var monsterPositionX4=getRandomNo ();
+   var monsterPositionY4=getRandomNo ();
    monster4.style.setProperty("top", monsterPositionY4 +"px" );
    monster4.style.setProperty("left", monsterPositionX4 +"px" ); 
 
   
     snakeHead.classList.add("snake-body");
     
-    var PositionX=0;
-    var PositionY=0;
+    var PositionX=100;
+    var PositionY=100;
 
 
     function pointsUp() {
-    monsterPositionX = Math.floor(Math.random()*49)*10;
-    monsterPositionY = Math.floor(Math.random()*49)*10;
+    monsterPositionX = getRandomNo ();
+    monsterPositionY = getRandomNo ();
     monster.style.setProperty("top", monsterPositionY +"px" );
     monster.style.setProperty("left", monsterPositionX +"px" );
     points = points+1;
@@ -101,15 +100,15 @@ document.addEventListener('keydown', function(event) {
 
      
      function pointsUp2() {
-        monsterPositionX2 = Math.floor(Math.random()*49)*10;
-        monsterPositionY2 = Math.floor(Math.random()*49)*10;
+        monsterPositionX2 = getRandomNo ();
+        monsterPositionY2 = getRandomNo ();
         monster2.style.setProperty("top", monsterPositionY2 +"px" );
         monster2.style.setProperty("left", monsterPositionX2 +"px" );
     points = points+1;
     }
     function pointsUp3() {
-        monsterPositionX3 = Math.floor(Math.random()*49)*10;
-        monsterPositionY3 = Math.floor(Math.random()*49)*10;
+        monsterPositionX3 = getRandomNo ();
+        monsterPositionY3 = getRandomNo ();
         monster3.style.setProperty("top", monsterPositionY3 +"px" );
         monster3.style.setProperty("left", monsterPositionX3 +"px" );
     points = points+1;
@@ -122,10 +121,6 @@ PositionX = PositionX + xDirection[xDirection.length -1];
 PositionY = PositionY + yDirection[yDirection.length -1];
 snakeHead.style.setProperty("top", PositionY +"px" );
 snakeHead.style.setProperty("left", PositionX +"px" );
-        
-        
-        
-        
         
 
 if (PositionX == -10 || PositionX == pgHeight)
@@ -145,7 +140,6 @@ if (PositionY == -10 || PositionY == pgWidth)
 if (PositionX == monsterPositionX && PositionY == (monsterPositionY)) 
 {
    pointsUp();
-   
 }
     
 
@@ -174,6 +168,7 @@ if (points > 19 && (PositionX == monsterPositionX3 && PositionY == (monsterPosit
 
 if (points > 9 && (PositionX == monsterPositionX4 && PositionY == (monsterPositionY4)))
 {
+    alert('GameOver! Red monster is bad!');
     gameOver();
 }  
 var i;
@@ -182,6 +177,19 @@ for (i = 0; i < x.length; i++) {
     x[i].style.setProperty("top", yPositionArr[yPositionArr.length -1 -i ]  +"px" );
     x[i].style.setProperty("left", xPositionArr[xPositionArr.length -1 -i ] +"px" );
 }
+var mY;
+var mX;
+
+for (var m = 0; m < x.length; m++){
+    mY = yPositionArr[yPositionArr.length -2 -m ]
+   mX = xPositionArr[xPositionArr.length -2 -m ]
+if (PositionX == mX && PositionY == mY){
+    alert("GameOver! Don't eat your tail!");
+gameOver();
+}}
+
+
+
 
         xPositionArr.push(PositionX); 
         yPositionArr.push(PositionY);
